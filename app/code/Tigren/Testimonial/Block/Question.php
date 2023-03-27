ead4658dbf5a3d919391057dfc50bca9c0775654
vanthanh <thanhbnvh@gmail.com>
2023-03-27 17:29:46 +0700

    Exercise 1
    Update module

--- app/code/Tigren/Testimonial/Block/Question.php
+++ app/code/Tigren/Testimonial/Block/Question.php
@@ -3,13 +3,22 @@
 namespace Tigren\Testimonial\Block;
 
 use Magento\Framework\View\Element\Template;
+use Tigren\Testimonial\Model\ResourceModel\Question\CollectionFactory;
 
 class Question extends Template
 {
+    protected $questionCollection;
 
-public function __construct(Template\Context $context, array $data = [])
-{
-    parent::__construct($context, $data);
-}
+    public function __construct(CollectionFactory $questionCollection, Template\Context $context, array $data = [])
+    {
+        $this->questionCollection = $questionCollection;
+        parent::__construct($context, $data);
+    }
 
+    public function getDataQuestion()
+    {
+        $data = $this->questionCollection->create();
+
+        return $data;
+    }
 }
