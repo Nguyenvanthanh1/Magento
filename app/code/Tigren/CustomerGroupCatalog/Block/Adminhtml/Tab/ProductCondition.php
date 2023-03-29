92db50cc30782a96ce85a5533e2ee01af5424943
vanthanh <thanhbnvh@gmail.com>
2023-03-29 17:56:27 +0700

    Exercise 2
    Update product condition

---
+++ app/code/Tigren/CustomerGroupCatalog/Block/Adminhtml/Tab/ProductCondition.php
@@ -0,0 +1,185 @@
+<?php
+/*
+ * @author  Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2023 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license  Open Software License (“OSL”) v. 3.0
+ */
+
+namespace Tigren\CustomerGroupCatalog\Block\Adminhtml\Tab;
+
+use Tigren\CustomerGroupCatalog\Controller\RegistryConstants;
+use Magento\Backend\Block\Widget\Form\Renderer\Fieldset;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Data\FormFactory;
+use Magento\SalesRule\Model\Rule;
+
+class ProductCondition extends \Magento\Backend\Block\Widget\Form\Generic implements
+    \Magento\Ui\Component\Layout\Tabs\TabInterface
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Backend\Block\Widget\Form\Renderer\Fieldset
+     */
+    protected $_rendererFieldset;
+
+    /**
+     * @var \Magento\Rule\Block\Conditions
+     */
+    protected $_conditions;
+
+    /**
+     * @var string
+     */
+    protected $_nameInLayout = 'conditions_apply_to';
+
+    /**
+     * @var \Magento\SalesRule\Model\RuleFactory
+     */
+    protected $ruleFactory;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param FormFactory $formFactory
+     * @param \Magento\Rule\Block\Conditions $conditions
+     * @param \Magento\Backend\Block\Widget\Form\Renderer\Fieldset $rendererFieldset
+     * @param array $data
+     * @param \Magento\SalesRule\Model\RuleFactory|null $ruleFactory
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        FormFactory $formFactory,
+        \Magento\Rule\Block\Conditions $conditions,
+        \Magento\Backend\Block\Widget\Form\Renderer\Fieldset $rendererFieldset,
+        array $data = [],
+    ) {
+        $this->rendererFieldset = $rendererFieldset;
+        $this->conditions = $conditions;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @codeCoverageIgnore
+     */
+    public function getTabClass()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTabUrl()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isAjaxLoaded()
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTabLabel()
+    {
+        return __('Product Conditions');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTabTitle()
+    {
+        return __('Product Conditions');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function canShowTab()
+    {
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+
+    /**
+     * Prepare form before rendering HTML
+     *
+     * @return $this
+     */
+    protected function _prepareForm()
+    {
+        $formName = 'customer_catalog_form';
+        /** @var \Tigren\CustomerGroupCatalog\Model\Rule $model */
+        $model = $this->_coreRegistry->registry(RegistryConstants::GROUPCAT_RULE_ID);
+        /** @var \Magento\Framework\Data\Form $form */
+        $form = $this->_formFactory->create();
+        $form->setHtmlIdPrefix('rule_');
+        $fieldset = $form->addFieldset(
+            'conditions_fieldset',
+            ['legend' => __('Conditions')]
+        );
+        $renderer = $this->rendererFieldset
+            ->setTemplate('Tigren_CustomerGroupCatalog::condition/fieldset.phtml')
+            ->setFieldSetId($model->getConditionsFieldSetId($formName))
+            ->setNewChildUrl(
+                $this->getUrl(
+                    'customer_catalog/rule/newConditionHtml/form/' . $model->getConditionsFieldSetId($formName),
+                    ['form_namespace' => $formName]
+                )
+            );
+        $fieldset->setRenderer($renderer);
+
+        $fieldset->addField(
+            'conditions',
+            'text',
+            [
+                'name'     => 'conditions',
+                'label'    => __('Product Conditions'),
+                'title'    => __('Product Conditions'),
+                'required' => true,
+                'data-form-part' => $formName
+            ]
+        )
+            ->setRule($model)
+            ->setRenderer($this->conditions);
+
+        $form->setValues($model->getData());
+        $this->setConditionFormName($model->getConditions(), $formName, $model->getConditionsFieldSetId($formName));
+        $this->setForm($form);
+        return parent::_prepareForm();
+    }
+
+    /**
+     * Handles addition of form name to condition and its conditions.
+     *
+     * @param \Magento\Rule\Model\Condition\AbstractCondition $conditions
+     * @param string $formName
+     * @return void
+     */
+    private function setConditionFormName(\Magento\Rule\Model\Condition\AbstractCondition $conditions, $formName)
+    {
+        $conditions->setFormName($formName);
+        if ($conditions->getConditions() && is_array($conditions->getConditions())) {
+            foreach ($conditions->getConditions() as $condition) {
+                $this->setConditionFormName($condition, $formName);
+            }
+        }
+    }
+}
--- app/code/Tigren/CustomerGroupCatalog/Model/Rule.php
+++ app/code/Tigren/CustomerGroupCatalog/Model/Rule.php
@@ -2,7 +2,7 @@
 
 namespace Tigren\CustomerGroupCatalog\Model;
 
-use Magento\Framework\Model\AbstractModel;
+use Magento\Rule\Model\AbstractModel;
 use Tigren\CustomerGroupCatalog\Model\ResourceModel\Rule as ResourceModel;
 
 class Rule extends AbstractModel
@@ -11,6 +11,21 @@ class Rule extends AbstractModel
      * @var string
      */
     protected $_eventPrefix = 'tigren_customer_group_catalog_rule_model';
+
+    public function getConditionsFieldSetId($formName = '')
+    {
+        return $formName . 'rule_conditions_fieldset_' . $this->getId();
+    }
+
+    public function getConditionsInstance()
+    {
+        return [];
+    }
+
+    public function getActionsInstance()
+    {
+        return [];
+    }
 
     /**
      * Initialize magento model.
