e9cae667fa72d8cc6147ed46c4d4a988614f0926
Thanh <thanh@example.com>
2023-04-05 00:39:10 +0700

    Exercise 2
    Update module Tigren Customer Group Catalog

--- app/code/Tigren/CustomerGroupCatalog/Model/ResourceModel/Rule.php
+++ app/code/Tigren/CustomerGroupCatalog/Model/ResourceModel/Rule.php
@@ -5,6 +5,10 @@ namespace Tigren\CustomerGroupCatalog\Model\ResourceModel;
 use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
+/**
+ * Class Rule
+ * @package Tigren\CustomerGroupCatalog\Model\ResourceModel
+ */
 class Rule extends AbstractDb
 {
     /**
@@ -12,14 +16,24 @@ class Rule extends AbstractDb
      */
     protected $_eventPrefix = 'tigren_customer_group_catalog_rule_resource_model';
 
+    /**
+     * @param $ruleId
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function getStoreByRuleId($ruleId)
     {
-        $result = $this->getConnection()->select()->from(['main_table' => $this->getMainTable()])->join(['st' => $this->getTable($this->getMainTable() . '_store')],
-            'main_table.rule_id=st.rule_id')->where('st.rule_id=?', $ruleId);
+        $result = $this->getConnection()->select()->from(['main_table' => $this->getMainTable()])->join(
+            ['st' => $this->getTable($this->getMainTable() . '_store')],
+            'main_table.rule_id=st.rule_id'
+        )->where('st.rule_id=?', $ruleId);
         return $this->filterData($this->getConnection()->fetchAll($result));
-
     }
 
+    /**
+     * @param $data
+     * @return array
+     */
     public function filterData($data)
     {
         $datas = [];
@@ -34,14 +48,24 @@ class Rule extends AbstractDb
         return $datas;
     }
 
+    /**
+     * @param $ruleId
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function getCustomerGroupByRuleId($ruleId)
     {
-        $result = $this->getConnection()->select()->from(['main_table' => $this->getMainTable()])->join(['cg' => $this->getTable($this->getMainTable() . '_group')],
-            'main_table.rule_id=cg.rule_id')->where('cg.rule_id=?', $ruleId);
+        $result = $this->getConnection()->select()->from(['main_table' => $this->getMainTable()])->join(
+            ['cg' => $this->getTable($this->getMainTable() . '_group')],
+            'main_table.rule_id=cg.rule_id'
+        )->where('cg.rule_id=?', $ruleId);
         return $this->filterData($this->getConnection()->fetchAll($result));
-
     }
 
+    /**
+     * @param AbstractModel $object
+     * @return Rule
+     */
     protected function _afterSave(AbstractModel $object)
     {
         $this->addGroupData($object);
@@ -49,52 +73,89 @@ class Rule extends AbstractDb
         return parent::_afterSave($object);
     }
 
+    /**
+     * @param $object
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function addGroupData($object)
     {
         $_prefixGroup = '_group';
         $tableGroup = $this->getMainTable() . $_prefixGroup;
-        $selectGroup = $this->getConnection()->select()->from($tableGroup)->where('rule_id=?', $object->getRuleId());
-        $checkRule = $this->getConnection()->fetchAll($selectGroup);
-        if (!empty($checkRule)) {
+        $connection = $this->getConnection();
+        $selectGroup = $connection->select()->from($tableGroup)->where('rule_id=?', $object->getRuleId());
+        $checkRule = $connection->fetchAll($selectGroup);
+        if (!empty($checkRule) && is_array($checkRule)) {
             foreach ($checkRule as $rule) {
-                if (isset($object['customer_group_ids']) && is_array($object['customer_group_ids'])) {
+                if (!in_array(
+                    $rule['customer_group_id'],
+                    $object['customer_group_ids']
+                ) && isset($object['customer_group_ids']) && is_array($object['customer_group_ids']) && !empty($object['customer_group_ids'])) {
+                    $connection->delete(
+                        $tableGroup,
+                        ['customer_group_id=?' => $rule['customer_group_id'], 'rule_id=?' => $object['rule_id']]
+                    );
+                }
+                if (isset($object['customer_group_ids']) && !empty($object['customer_group_ids']) && is_array($object['customer_group_ids'])) {
                     foreach ($object['customer_group_ids'] as $groupId) {
-                        if ($rule['customer_group_id'] === $groupId) {
-                            $this->getConnection()->insertOnDuplicate($tableGroup,
-                                ['customer_group_id' => $groupId, 'rule_id' => $object->getId()]);
-                        }
-                        if ($rule['rule_id'] === $object['rule_id'] && $rule['customer_group_id'] !== $groupId) {
-                            $this->getConnection()->delete($tableGroup, ['customer_group_id' => $rule['customer_group_id']]);
-                        }
-                            $this->getConnection()->insertOnDuplicate($tableGroup,
-                                ['customer_group_id' => $groupId, 'rule_id' => $object->getId()]);
+                        $this->getConnection()->insertOnDuplicate(
+                            $tableGroup,
+                            ['customer_group_id' => $groupId, 'rule_id' => $object->getId()]
+                        );
                     }
+                }
+            }
+        } else {
+            if (isset($object['customer_group_ids']) && !empty($object['customer_group_ids']) && is_array($object['customer_group_ids'])) {
+                foreach ($object['customer_group_ids'] as $groupId) {
+                    $this->getConnection()->insert(
+                        $tableGroup,
+                        ['customer_group_id' => $groupId, 'rule_id' => $object->getId()]
+                    );
                 }
             }
         }
     }
 
+    /**
+     * @param $object
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function addStoreData($object)
     {
         $_prefixStore = '_store';
         $tableStore = $this->getMainTable() . $_prefixStore;
         $selectStore = $this->getConnection()->select()->from($tableStore)->where('rule_id=?', $object->getRuleId());
         $checkRule = $this->getConnection()->fetchAll($selectStore);
-        if (!empty($checkRule)) {
+
+        if (!empty($checkRule) && is_array($checkRule)) {
             foreach ($checkRule as $rule) {
-                if (isset($object['store_ids']) && is_array($object['store_ids'])) {
-                    foreach ($object['store_ids'] as $storeId) {
-                        if ($rule['store_id'] === $storeId) {
-                            $this->getConnection()->insertOnDuplicate($tableStore,
-                                ['store_id' => $storeId, 'rule_id' => $object->getId()]);
-                        }
-                        if ($rule['rule_id'] === $object['rule_id'] && $rule['store_id'] !== $storeId) {
-                            $this->getConnection()->delete($tableStore, ['store_id' => $storeId]);
-                        } else {
-                            $this->getConnection()->insertOnDuplicate($tableStore,
-                                ['store_id' => $storeId, 'rule_id' => $object->getId()]);
-                        }
-                    }
+                if (!in_array(
+                    $rule['store_id'],
+                    $object['store_ids']
+                ) && isset($object['store_ids']) && is_array($object['store_ids']) && !empty($object['store_ids'])) {
+                    $this->getConnection()->delete(
+                        $tableStore,
+                        ['store_id=?' => $rule['store_id'], 'rule_id=?' => $object->getRuleId()]
+                    );
+                }
+            }
+            if (isset($object['store_ids']) && is_array($object['store_ids']) && !empty($object['store_ids'])) {
+                foreach ($object['store_ids'] as $storeId) {
+                    $this->getConnection()->insertOnDuplicate(
+                        $tableStore,
+                        ['store_id' => $storeId, 'rule_id' => $object->getId()]
+                    );
+                }
+            }
+        } else {
+            if (isset($object['store_ids']) && !empty($object['store_ids']) && is_array($object['store_ids'])) {
+                foreach ($object['store_ids'] as $storeId) {
+                    $this->getConnection()->insert(
+                        $tableStore,
+                        ['store_id' => $storeId, 'rule_id' => $object->getId()]
+                    );
                 }
             }
         }
