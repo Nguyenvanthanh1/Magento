94eeb3e9cd376215e2f0427ca0828ca13eb932a9
Thanh <thanh@example.com>
2023-04-21 00:41:04 +0700

    MiniProject
    Update Mini Project

---
+++ app/code/Tigren/AdvancedCheckout/Plugin/Checkout/LayoutProcessorPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tigren\AdvancedCheckout\Plugin\Checkout;
+
+class LayoutProcessorPlugin
+{
+    /**
+     * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
+     * @param array $jsLayout
+     * @return array
+     */
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array  $jsLayout
+    ) {
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+        ['shippingAddress']['children']['before-shipping-method-form']['children']['delivery_date'] = [
+            'component' => 'Magento_Ui/js/form/element/date',
+            'config' => [
+                'customScope' => 'shippingAddress',
+                'template' => 'ui/form/field',
+                'elementTmpl' => 'ui/form/element/date',
+                'options' => [],
+                'id' => 'delivery_date'
+            ],
+            'dataScope' => 'shippingAddress.delivery_date',
+            'label' => __('Delivery Date'),
+            'provider' => 'checkoutProvider',
+            'visible' => true,
+            'validation' => [],
+            'sortOrder' => 800,
+            'id' => 'delivery_date'
+        ];
+        return $jsLayout;
+    }
+}
